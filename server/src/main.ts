32e11a9d6bd20b294f0d98d4ee62dd65e2c25ae0
João Paulo Dias <joaopdaisventura@gmail.com>
2024-06-13 12:15:49 -0300

    fix: enabling the cors protocol

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,17 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
 	app.useGlobalPipes(new ValidationPipe());
+
+	const corsOptions = {
+		origin: [process.env.FRONTEND],
+		methods: ["GET", "DELETE", "POST", "PUT"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	};
+
+	app.enableCors(corsOptions);
+	
 	await app.listen(3000);
 }
 bootstrap();
