8076df7767a244be71c850890702c07d8608dddb
João Paulo Dias <joaopdiasventura@gmail.com>
2024-06-12 21:34:56 -0300

    remove: removing an unused service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
-import { AppService } from "./app.service";
 import { UserModule } from "./user/user.module";
 import { FileModule } from "./file/file.module";
 
 @Module({
 	imports: [UserModule, FileModule],
 	controllers: [],
-	providers: [AppService],
+	providers: [],
 })
 export class AppModule {}
